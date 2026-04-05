7d7adb6ce38b94419fb06113affa5f93917f2f0f
Douglas Rivas <64998568+colcel2005@users.noreply.github.com>
2026-04-05 16:21:16 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro-mascotas.pages.dev/
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://creativetimofficial.github.io',
-  base: '/astro-ecommerce'
+  site: 'https://astro-mascotas.pages.dev',
+  base: '/'
 });
